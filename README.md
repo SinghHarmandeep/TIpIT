dccbfd1a636339622f4dc640cf8b1c8ee5db2ca7
Harmandeep Singh <deepharman2000@gmail.com>
2019-01-04 02:03:48 -0500

    adding gif

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ Here's a walkthrough of implemented user stories:
 <img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
+![](tipit.gif)
 
 ## Notes
 
--- tipIT/ViewController.swift
+++ tipIT/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //billField.becomeFirstResponder()
+        billField.becomeFirstResponder()
     }
 
     //the tap gesture to quit the keyboard
